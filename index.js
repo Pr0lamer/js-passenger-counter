77fa2a78c6d14f8f472f42ac765b696109aba722
Pr0lamer <andrewmakmail@gmail.com>
2022-02-14 12:44:33 +0200

    listening to the increment button click

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.getElementById("count-el").innerText = 5;
+let count = 0;
+let countEl = document.getElementById("count-el");
+
+function increment() {
+    count++;
+    countEl.innerText = count;
+}
